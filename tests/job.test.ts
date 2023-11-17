adeccd2b45a5106b14f15537a7a66a92b929164b
Donald Pakkies <donaldpakkies@gmail.com>
2023-11-17 20:58:08 +0200

    feat: add tests for sync

--- tests/job.test.ts
+++ tests/job.test.ts
@@ -4,6 +4,8 @@ import { Redis } from '@formidablejs/framework'
 import { Application } from './app'
 import { queue } from "../src"
 import { LogMessage } from './app/LogMessage'
+import { SayHello } from './app/SayHello'
+import { JobExecutionTimeoutException } from '../src/Errors/JobExecutionTimeoutException'
 
 describe('src/Queue', () => {
     beforeAll(async () => {
@@ -42,5 +44,25 @@ describe('src/Queue', () => {
         const job = await LogMessage.delay('1 minute').dispatch()
 
         expect(job.options.delay).toBeDefined()
+    })
+
+    it('should queue a sync job', async () => {
+        const log = jest.spyOn(global.console, 'log').mockImplementation(() => { });
+        const job = await SayHello.dispatch('Donald')
+
+        expect(log).toHaveBeenCalledWith('Hello Donald')
+        expect(job).toBeUndefined()
+
+        log.mockRestore();
+    })
+
+    it('should queue a sync delayed job', async () => {
+        const log = jest.spyOn(global.console, 'log').mockImplementation(() => { });
+        const job = await SayHello.delay('10 seconds').dispatch()
+
+        expect(log).toHaveBeenCalledWith('this logs a message...')
+        expect(job).toBeUndefined()
+
+        log.mockRestore();
     })
 })
